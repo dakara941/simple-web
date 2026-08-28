1d53b3ef55210252b9b253cb43671aa4f5301116
dakara941 <oojima166@gmail.com>
2026-08-28 14:18:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,13 @@
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# 確保這整個專案裡，只有一個 @app.route('/') 且只有一個 def home():
 @app.route('/')
 def home():
-    # 預留：未來可以回傳 HTML 網頁 (如 index.html)
-    return jsonify({
-        "status": "success",
-        "message": "Flask 作品集成功部署！"
-        
-    })
-
-from flask import Flask, render_template  # 引入 render_template
-
-@app.route('/')
-def home():
-    return render_template('index.html')  # 渲染 HTML 畫面
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # 雲端平台分配的 Port，找不到時（如本地測試）預設為 5000
     port = int(os.environ.get("PORT", 5000))
-    # 必須綁定在 0.0.0.0，平台才能從外部對接流量
     app.run(host='0.0.0.0', port=port)
